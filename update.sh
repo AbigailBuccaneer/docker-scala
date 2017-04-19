811343394924b80a8b98ff2e04dead16568294ec
AbigailBuccaneer <AbigailBuccaneer@users.noreply.github.com>
2017-04-19 12:15:55 +0100

    Download .deb over https

--- update.sh
+++ update.sh
@@ -39,7 +39,7 @@ function generate_dockerfile {
 		FROM $base_image
 
 		ENV SCALA_VERSION $version
-		RUN wget -q "http://www.scala-lang.org/files/archive/scala-\$SCALA_VERSION.deb" && \\
+		RUN wget -q "https://www.scala-lang.org/files/archive/scala-\$SCALA_VERSION.deb" && \\
 		    ( dpkg -i "scala-\$SCALA_VERSION.deb" || true ) && \\
 		    apt-get update -y && apt-get install -y -f --no-install-recommends && \\
 		    rm "scala-\$SCALA_VERSION.deb" && \\
